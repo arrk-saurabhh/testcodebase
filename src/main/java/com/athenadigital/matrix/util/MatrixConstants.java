7f67945318f6eee052b0e8131fd0646004aed21a
arrk-saurabhh <saurabh.herwadkar@arrkgroup.com>
2013-08-15 20:10:08 +0530

    First commit

---
+++ src/main/java/com/athenadigital/matrix/util/MatrixConstants.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.athenadigital.matrix.util;
+
+/**
+ * @author Admin
+ *
+ */
+public class MatrixConstants {
+
+	/**
+	 * 
+	 */
+	public MatrixConstants() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/test/java/com/athenadigital/matrix/util/TestMatrixConstants.java
@@ -0,0 +1,10 @@
+package com.athenadigital.matrix.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestMatrixConstants {
+
+
+}
